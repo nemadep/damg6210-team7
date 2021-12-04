63fcbc184e25c1656265320fceb92991784c3a85
prathamnemade <prathameshnemade95@gmail.com>
2021-12-03 21:12:38 -0500

    students addition function and procedure

--- Project/Prathamesh/student_insert_script.sql
+++ Project/Prathamesh/student_insert_script.sql
@@ -1,58 +1,3 @@
-CREATE OR REPLACE PROCEDURE p_makestudentaresident (
-    studuentid NUMBER,
-    dormname   VARCHAR,
-    from_date  DATE,
-    to_date    DATE
-) IS
-    is_available      NUMBER;
-    temp_dorm_id      NUMBER;
-    is_dorm_available NUMBER;
-    e_dorm_valid EXCEPTION;
-BEGIN
-    SELECT
-        COUNT(*)
-    INTO is_available
-    FROM
-        dorm
-    WHERE
-        lower(name) = lower(dormname);
-
-    IF is_available = 1 THEN
-        --check availability-check availability-check availability-check availability
-        SELECT
-            dorm_id
-        INTO temp_dorm_id
-        FROM
-            dorm
-        WHERE
-            lower(name) = lower(dormname);
-
-        is_dorm_available(temp_dorm_id, from_date, to_date);
-        INSERT INTO resident (
-            dorm_id,
-            student_id,
-            to_date,
-            from_date
-        ) VALUES (
-            temp_dorm_id,
-            studuentid,
-            to_date,
-            from_date
-        );
-
-    ELSE
-        RAISE e_dorm_valid;
-    END IF;
-
-EXCEPTION
-    WHEN dorm_valid THEN
-        dbms_output.put_line('Invalid dorm!');
-END;
-/
-
-
-
-
 CREATE OR REPLACE FUNCTION f_check_dorm_availability (
     dormid   IN NUMBER,
     fromdate IN DATE,
@@ -64,7 +9,7 @@ CREATE OR REPLACE FUNCTION f_check_dorm_availability (
 BEGIN
     SELECT
         dorm_capacity
-    INTO dorm_availability
+    INTO dorm_capacity
     FROM
         dorm
     WHERE
@@ -86,3 +31,61 @@ BEGIN
     RETURN is_available;
 END f_check_dorm_availability;
 /
+
+
+CREATE OR REPLACE PROCEDURE p_makestudentaresident (
+    studuentid NUMBER,
+    dormname   VARCHAR,
+    from_date  DATE,
+    to_date    DATE
+) IS
+    is_available      NUMBER;
+    temp_dorm_id      NUMBER;
+    temp_is_dorm_available NUMBER;
+    e_dorm_valid EXCEPTION;
+BEGIN
+    SELECT
+        COUNT(*)
+    INTO is_available
+    FROM
+        dorm
+    WHERE
+        lower(dorm_name) = lower(dormname);
+
+    IF is_available = 1 THEN
+        --check availability-check availability-check availability-check availability
+        SELECT
+            dorm_id
+        INTO temp_dorm_id
+        FROM
+            dorm
+        WHERE
+            lower(dorm_name) = lower(dormname);
+
+        temp_is_dorm_available := f_check_dorm_availability(temp_dorm_id, from_date, to_date);
+        IF temp_is_dorm_available = 1 THEN 
+            INSERT INTO resident (
+                dorm_id,
+                student_id,
+                to_date,
+                from_date
+            ) VALUES (
+                temp_dorm_id,
+                studuentid,
+                to_date,
+                from_date
+            );
+        ELSE
+            dbms_output.put_line('Dorm not available!');
+        END IF;
+    ELSE
+        RAISE e_dorm_valid;
+    END IF;
+
+EXCEPTION
+    WHEN e_dorm_valid THEN
+        dbms_output.put_line('Invalid dorm!');
+END;
+/
+
+
