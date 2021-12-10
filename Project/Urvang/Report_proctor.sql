f765b357439d2f788f510972d08a114ce471ff53
Urvang <patel.ur@northeastern.edu>
2021-12-10 00:40:18 -0500

    Added procotr report

---
+++ Project/Urvang/Report_proctor.sql
@@ -0,0 +1,28 @@
+/*Report to find the proctor scheduled with max number of shifts*/
+
+CREATE OR REPLACE VIEW proctorwithmaxshifts AS
+    WITH shift_dist AS (
+        SELECT
+            proctor_id,
+            COUNT(*) shift_count
+        FROM
+            shifts
+        GROUP BY
+            proctor_id
+    )
+    SELECT
+        *
+    FROM
+        (
+            SELECT
+                distribution.proctor_id,
+                proc_details.proctor_name,
+                distribution.shift_count
+            FROM
+                     proctor proc_details
+                JOIN shift_dist distribution ON proc_details.proctor_id = distribution.proctor_id
+            ORDER BY
+                shift_count DESC
+        )
+    WHERE
+        ROWNUM <= 10;
