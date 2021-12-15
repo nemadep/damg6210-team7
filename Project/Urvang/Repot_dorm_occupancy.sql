d7731182ef50eed46b659908e246ee7b28f3b931
Urvang <patel.ur@northeastern.edu>
2021-12-15 04:16:28 -0500

    Created dorm occupancy report

---
+++ Project/Urvang/Repot_dorm_occupancy.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE VIEW dorm_occupancy AS
+    WITH dorm_cap AS (
+        SELECT
+            dorm_id,
+            dorm_capacity
+        FROM
+            dorm
+    ), dorm_occ AS (
+        SELECT
+            res.dorm_id,
+            COUNT(*) dorm_count
+        FROM
+            resident res
+        WHERE
+            to_date >= sysdate
+        GROUP BY
+            res.dorm_id
+    )
+    SELECT
+        dc.dorm_id,
+        nvl((dc.dorm_capacity - do.dorm_count), dc.dorm_capacity) AS rooms_left
+    FROM
+        dorm_cap dc
+        LEFT JOIN dorm_occ do ON dc.dorm_id = do.dorm_id;
+
+SELECT
+    *
+FROM
+    dorm_occupancy;
